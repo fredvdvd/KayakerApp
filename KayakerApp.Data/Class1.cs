53dedf7b947f92e73673658a92de691cf645252e
Frederik Vandervelden <fredvdvd@gmail.com>
2019-03-19 00:59:19 +0100

    adding DBContext

--- KayakerApp.Data/Class1.cs
+++ KayakerApp.Data/Class1.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using KayakerApp.Domain;
 
 namespace KayakerApp.Data
 {
-    public class Class1
+    public class KayakerContext:DbContext
     {
+        public DbSet<Kayaker> Kayakers { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
     }
 }
